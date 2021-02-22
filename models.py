42943e881343dfc4b6999616ac7f6740efe6b310
John Curry <jtc3852@gmail.com>
2021-02-21 21:29:33 -0500

    Add basic styling

--- models.py
+++ models.py
@@ -6,6 +6,7 @@ db = SQLAlchemy()
 
 default_img = "https://www.freeiconspng.com/uploads/icon-user-blue-symbol-people-person-generic--public-domain--21.png"
 
+
 def connect_db(app):
     db.app = app
     db.init_app(app)
@@ -18,19 +19,24 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     first_name = db.Column(db.Text, nullable=False)
     last_name = db.Column(db.Text, nullable=False)
-    image_url = db.Column(db.Text, nullable = False, default = default_img)
+    image_url = db.Column(db.Text, nullable=False, default=default_img)
 
     posts = db.relationship("Post", backref="user")
 
 
 class Post(db.Model):
     """Posts"""
-    __tablename__= "post"
+    __tablename__ = "post"
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.Text, nullable=False)
     content = db.Column(db.Text, nullable=False)
-    created_at = db.Column(db.DateTime, nullable=False, default=datetime.datetime.now)
+    created_at = db.Column(db.DateTime, nullable=False,
+                           default=datetime.datetime.now)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
 
-    
+    @property
+    def friendly_date(self):
+        """Return nicely-formatted date."""
+
+        return self.created_at.strftime("%m/%d/%y")
